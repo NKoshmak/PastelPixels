5d54803ac40786214d52e019d14f8e1a5828cd61
Nataliia <koshmaknatalia@gmail.com>
2025-10-20 18:46:12 -0600

    Fixed mobile scroll

--- components/SmoothScrollProvider.js
+++ components/SmoothScrollProvider.js
@@ -2,8 +2,19 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 
 function SmoothScrolling({ children }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    setIsMobile(window.matchMedia("(pointer: coarse)").matches);
+  }, []);
+
+  if (isMobile) {
+    return <>{children}</>; // 👈 native scroll fallback
+  }
+
+  
   return (
-    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: true }}>
+    <ReactLenis root options={{ lerp: 0.1, duration: 1.5, smoothTouch: false,  orientation: "vertical", }}>
       {children}
     </ReactLenis>
   );
